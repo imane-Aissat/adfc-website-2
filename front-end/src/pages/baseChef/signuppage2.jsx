35af0b961cbf0a1297ec05a0be13530e909f4f87
imeney <imaneaissatpr@gmail.com>
2025-04-20 17:06:48 +0100

    sign up 2 and login

---
+++ front-end/src/pages/baseChef/signuppage2.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import '..//../style/signupPage.css';
+
+function SignupPageTwo() {
+    return(
+        <div className = "signup-page">
+            <div className="picture-signup-form">
+                <img id= "workers-image-signup" src="/images/picture-signup1.jpg" alt="workers on site" />
+            </div>
+            <div className="signup-form-area">
+                <div id="saisie-infos-signup-box">
+                    <span id="saisie-infos-signup">2. Saisissez vos informations</span>
+                </div>
+                <form id="signup-form" action="">
+                    <input type="text" className="signup-form-input-text" placeholder="Nom" />
+                    <input type="text" className="signup-form-input-text" placeholder="Prénom" />
+                    <input type="text" className="signup-form-input-text" placeholder="Email" />
+                    <input type="text" className="signup-form-input-text" placeholder="Mot de passe" />
+                    <input type="text" className="signup-form-input-text" placeholder="Confirmer le mot de passe" />
+
+                    <button id="create-account-button" type="submit">Créer</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default SignupPageTwo;
